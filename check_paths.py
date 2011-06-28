e969314f7907f928445f14eb447fb521e71455ce
Jon Schlueter <jon.schlueter@gmail.com>
2011-06-28 07:07:51 -0400

    Added check_paths script
    checks for paths for files of the type mp3
    if the path is not found in the DB remove records for that path from the
    DB

---
+++ check_paths.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# GPL V3 Licenced
+#
+# Jon Schlueter <jon.schlueter@gmail.com>
+#
+
+import sqlite3
+import sys,os
+
+con = sqlite3.connect('index_stuff.db')
+
+cursor = con.execute('SELECT distinct path from file_list where type = "mp3"')
+
+for (path) in cursor:
+    if not os.path.isdir(path[0]):
+        print "[%s] is missing removing entries"%path[0]
+        con.execute('delete from file_list where path = "%s"'%path)
+
