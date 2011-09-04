7eaaa86a6da810199c73096bc774dc43589cb943
Jon Schlueter <jon.schlueter@gmail.com>
2011-09-04 12:36:22 -0400

    actually delete the path from the DB on check_paths.py

--- check_paths.py
+++ check_paths.py
@@ -14,5 +14,6 @@ cursor = con.execute('SELECT distinct path from file_list where type = "mp3"')
 for (path) in cursor:
     if not os.path.isdir(path[0]):
         print "[%s] is missing removing entries"%path[0]
-        con.execute('delete from file_list where path = "%s"'%path)
+        con.execute( 'delete from file_list where path = "'+path[0]+'"')
+        con.commit()
 
