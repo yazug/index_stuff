edb69816e85b2a0cf815bb28abb771c4f14e6119
Jon Schlueter <jon.schlueter@gmail.com>
2012-10-14 14:33:32 -0400

    update create_db script
    added index on md5sum
    added table to keep track of detected duplicate files

--- create_db.py
+++ create_db.py
@@ -15,7 +15,10 @@ if con:
     con.execute('CREATE TABLE IF NOT EXISTS file_list (path TEXT, filename TEXT, type TEXT, size NUMERIC, mdate INTEGER, cdate INTEGER, md5sum TEXT)')
     con.execute('CREATE INDEX IF NOT EXISTS index_file_list_type on file_list (type)')
     con.execute('CREATE INDEX IF NOT EXISTS index_file_list_path on file_list (path)')
+    con.execute('CREATE INDEX IF NOT EXISTS index_file_list_md5sum on file_list (md5sum)')
     con.execute('CREATE TABLE IF NOT EXISTS folder_list(path TEXT, foldername TEXT, folder_count integer, file_count integer, mdate INTEGER, cdate INTEGER, last_checked DATE)')
+
+    con.execute('CREATE TABLE IF NOT EXISTS duplicate_list (md5sum TEXT, type TEXT, size NUMERIC, count NUMERIC)')
 
     con.commit()
     con.close()
