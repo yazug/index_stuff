ff461a0940e8af1dad58b703a4a89265e23f270e
Jon Schlueter <jon.schlueter@gmail.com>
2012-10-14 14:19:38 -0400

    fixup create_db to run and have a bit of error checking

--- create_db.py
+++ create_db.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 # GPL V3 licence
 # Jon Schlueter <jon.schlueter@gmail.com>
-# This script will create an empty DB which can be used by the index_stuff.py script
+# This script will create an empty DB which can be used by the index_stuff.py
 
 import sqlite3
 
+db_filename = 'index_stuff.db'
 
-con = sqlite3.connect("index_stuff.db")
+con = sqlite3.connect(db_filename)
 
-sql = "CREATE TABLE if not exists file_list (path TEXT, filename TEXT, type TEXT, size NUMERIC, mdate INTEGER, cdate INTEGER, md5sum TEXT);"
+if con:
+    print "Created [%s] db to hold things" % (db_filename,)
 
-con.execute(sql)
+    con.execute('CREATE TABLE IF NOT EXISTS file_list (path TEXT, filename TEXT, type TEXT, size NUMERIC, mdate INTEGER, cdate INTEGER, md5sum TEXT)')
+    con.execute('CREATE INDEX IF NOT EXISTS index_file_list_type on file_list (type)')
+    con.execute('CREATE INDEX IF NOT EXISTS index_file_list_path on file_list (path)')
+    con.execute('CREATE TABLE IF NOT EXISTS folder_list(path TEXT, foldername TEXT, folder_count integer, file_count integer, mdate INTEGER, cdate INTEGER, last_checked DATE)')
+
+    con.commit()
+    con.close()
 
 
-con.execute("create index if not exists index_file_list_type on file_list (type);")
-con.execute("create index if not exists index_file_list_path on file_list (path);")
-con execute("CREATE TABLE folder_list(path TEXT, foldername TEXT, folder_count integer, file_count integer, mdate INTEGER, cdate INTEGER, last_checked DATE);")
-
-
-
-con.commit();
-
-con.close();
+else:
+    print "Failed to open DB Connection to [%s]" % (db_filename,)
